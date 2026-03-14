589c3424614deb347dff3f76e2f49996eb8b0d2c
sai-sriram23 <saisrirambussa999@gmail.com>
2026-03-14 10:32:38 +0530

    fro

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8085',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8081',
     headers: {
         'Content-Type': 'application/json',
     },
